c30ad40dff29e3bd95d55fc5b770ee555cad6652
ealonsol <ealonsol@hotmail.com>
2017-07-23 23:01:49 -0500

    Second commit

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-var express = require('express');
-var app = express();
-app.get('/', (req, res)=>{
-  res.send('hello world');
+const express = require('express');
+const app = express();
+const mongoose= require('mongoose');
+const config = require('./config/database');
+
+
+console.log('la base de datos es : '+config.db);
+console.log('la ruta es : '+config.uri);
+mongoose.Promise = global.Promise;
+
+
+mongoose.connect(config.uri, (err)=>{
+  if(err){
+    console.log('No conectado a la base de datos ' + config.db, err);
+  }
+  else{
+    console.log('Conectado a la base de datos'+ config.db );
+  }
+});
+
+app.get('*', (req, res)=>{
+  res.send('<h1>hello world</h1>');
 });
 
 app.listen(3000,()=>{
