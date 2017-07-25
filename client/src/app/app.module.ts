bbd07c9e4683b118879e6c59e0c6f816a50e46c2
ealonsol <ealonsol@hotmail.com>
2017-07-24 22:23:20 -0500

    commit angular

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { AppRountingModule } from './app.routing.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
@@ -19,6 +20,8 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     BrowserModule,
     AppRountingModule
 =======
+=======
+>>>>>>> 97a3c550215d0979dd0fdf6d6f239af69b3ffc5f
 
 @NgModule({
   declarations: [
@@ -26,6 +29,9 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule
+<<<<<<< HEAD
+>>>>>>> 97a3c550215d0979dd0fdf6d6f239af69b3ffc5f
+=======
 >>>>>>> 97a3c550215d0979dd0fdf6d6f239af69b3ffc5f
   ],
   providers: [],
