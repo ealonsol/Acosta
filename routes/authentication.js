34506367cefedbb171dfa6ebf282975b054951fe
ealonso <ealonsol@hotmail.com>
2017-07-25 17:31:58 -0500

    potsman commit

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,6 +1,17 @@
-const User = require('../models/user');
-
 module.exports = (router) => {
+
+    const User = require('../models/user');
+
+    router.use(function(req, res, next) {
+        // do logging
+        console.log('something is happening.');
+        next(); // make sure we go to our next route and don't stop here
+    });
+
+    router.get('/', function(req, res) {
+        res.json({ message: 'hooray! welcome to our api!' });   
+    });
+
     router.post('/register', (req,res) => {
         /*req.body.email;
         req.body.username;
@@ -9,7 +20,36 @@ module.exports = (router) => {
             res.json({success:false, message:'No proveyo un email'})
         }
         else{
-            res.send('hello word');
+            if(!req.body.username){
+                res.json({success:false, message:'No proveyo un username'})
+            }
+            else{
+                if(!req.body.password){
+                    res.json({success:false, message:'No proveyo un password'})
+                }
+                else{
+                    res.send('Bienvenido ' +req.body.username);
+                    console.log(req.body.username);
+                    console.log(req.body.password);
+                    console.log(req.body.email);
+                    let user = new User({
+                        username: req.body.username.toLowerCase(),
+                        password:req.body.password.toLowerCase(),
+                        email:req.body.email
+                    });
+                    user.save((err)=>{
+                        if(err){
+                            res.json({sucess:false, message:'No se grabó el usuario. Error: ', err});
+                            res.send();
+                        }
+                        else{
+                            res.json({sucess:true, message:'Se grabó el usuario '});
+                            res.send();
+                        }
+                    });
+                    
+                }
+            }
         }
         
     });
